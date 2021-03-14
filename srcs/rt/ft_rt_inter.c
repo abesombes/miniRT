62ea003c161059c61a437c8e43c93eaa904507c1
abesombes <abesombes@protonmail.com>
2021-03-14 10:13:38 +0100

    0.17 intersect 1 sph works!

--- srcs/rt/ft_rt_inter.c
+++ srcs/rt/ft_rt_inter.c
@@ -6,7 +6,7 @@
 /*   By: abesombe <abesombe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 16:19:31 by abesombe          #+#    #+#             */
-/*   Updated: 2021/03/13 22:47:29 by abesombe         ###   ########.fr       */
+/*   Updated: 2021/03/13 23:32:21 by abesombe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,29 @@
 int		ft_rt_inter(t_ray *r, t_sphere *sp, t_inter *inter)
 {
 	inter->a = 1;
-	printf("\na: [%f]",inter->a);
+	// printf("\na: [%f]",inter->a);
 	inter->b = 2 * ft_vec_mul(&r->dir, ft_vec_sub(&r->orig, &sp->orig));
-	printf("\nb: [%f]",inter->b);
+	// printf("\nb: [%f]",inter->b);
 	inter->c = ft_vec_sqnorm(ft_vec_sub(&r->orig, &sp->orig)) - pow(sp->radius, 2);
-	printf("\nc: [%f]",inter->c);
+	// printf("\nc: [%f]",inter->c);
 	inter->delta = inter->b * inter->b - 4 * inter->a * inter->c;
-	printf("\ndelta: [%f]",inter->delta);
+	// printf("\ndelta: [%f]",inter->delta);
 	if (inter->delta < 0)
 		return (0);
 	inter->t1 = (-inter->b - sqrt(inter->delta))  / (2 * inter->a);
-	printf("\nt1: [%f]",inter->t1);
+	// printf("\nt1: [%f]",inter->t1);
 	inter->t2 = (-inter->b + sqrt(inter->delta))  / (2 * inter->a);
-	printf("\nt2: [%f]",inter->t2);
+	// printf("\nt2: [%f]",inter->t2);
 	if (inter->t2 < 0)
 		return (0);
 	if (inter->t1 > 0)
 		inter->t = inter->t1;
 	else
 		inter->t = inter->t2;
-	printf("\nt: [%f]",inter->t);
+	// printf("\nt: [%f]",inter->t);
 	inter->p = *ft_vec_add(&r->orig, ft_vec_mul_scal(&r->dir, inter->t));
-	ft_display_vec(&inter->p);
+	// ft_display_vec(&inter->p);
 	inter->n = *ft_vec_normvec(ft_vec_sub(&inter->p, &sp->orig));
-	ft_display_vec(&inter->n);
+	// ft_display_vec(&inter->n);
 	return (1);
 }
--- srcs/rt/ft_rt_trace_rays.c
+++ srcs/rt/ft_rt_trace_rays.c
@@ -6,7 +6,7 @@
 /*   By: abesombe <abesombe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 15:21:08 by abesombe          #+#    #+#             */
-/*   Updated: 2021/03/13 23:05:13 by abesombe         ###   ########.fr       */
+/*   Updated: 2021/03/13 23:34:08 by abesombe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,23 +35,16 @@ void ft_rt_trace_rays(t_scene *sc, t_inter *inter)
 		i = 0;
 		while (i < sc->res_w)
 		{
-			ft_vec_set(&sc->ray.dir, i - sc->res_w / 2, j - sc->res_h / 2, -sc->res_w / (2 * tan(cur_c.fov / 2)));
+			ft_vec_set(&sc->ray.dir, i - sc->res_w / 2, j - sc->res_h / 2, -sc->res_w / (2 * tan(cur_c.fov / 200)));
 			ft_vec_norm(&sc->ray.dir);
 			ft_vec_cpy(&sc->ray.orig, &cur_c.pos);
 			ft_ray_set(&sc->ray, &sc->ray.orig, &sc->ray.dir);
 			ft_vec_set(&sc->pix_int, 0., 0., 0.);
 			ft_init_inter(inter);
-			printf("\n-------------------------------------------------------\n");
-			printf("-------------------------------------------------------\n");
-			printf("\n[%i, %i]", i, j);
-			printf("\nNORMED RAY DIR:");
-			ft_display_vec(&sc->ray.dir);
 			inter->has_junc = ft_rt_inter(&sc->ray, &cur_s, inter);
 			if (inter->has_junc)
 			{
-				printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-				printf("\nintersect @ [%i, %i]", i, sc->res_h - j - 1);
-				sc->pix_int = *ft_vec_mul_scal(&sp_obj->rgb, 100000000 * cur_l.intst * fmax(0, ft_vec_mul(ft_vec_normvec(ft_vec_sub(&cur_l.pos, &inter->p)), &inter->n) / ft_vec_sqnorm(ft_vec_sub(&cur_l.pos, &inter->p))));
+				sc->pix_int = *ft_vec_mul_scal(&sp_obj->rgb, 20000000 * cur_l.intst * fmax(0, ft_vec_mul(ft_vec_normvec(ft_vec_sub(&cur_l.pos, &inter->p)), &inter->n) / ft_vec_sqnorm(ft_vec_sub(&cur_l.pos, &inter->p))));
 			}
 			sc->pix_color = ((int)round(fmin(fmax(pow(sc->pix_int.x, 0.4545), 0), 255)) + 0) << 16 | ((int)round(fmin(fmax(pow(sc->pix_int.y, 0.4545), 0), 255)) + 0) << 8 | ((int)round(fmin(fmax(pow(sc->pix_int.z, 0.4545), 0), 255)));
 			ft_render_pixel_put(sc, i, sc->res_h - j - 1, sc->pix_color);
